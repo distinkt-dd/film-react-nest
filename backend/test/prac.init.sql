a208253896274b6c35d7ded8f9cbee54e4bc2e78
barawek55 <barawek55@gmail.com>
2026-07-20 20:03:37 +0500

    fix(sql) фикс sql скриптов

--- backend/test/prac.init.sql
+++ backend/test/prac.init.sql
@@ -10,7 +10,7 @@ create table public.films
             primary key,
     rating      double precision                not null,
     director    varchar                         not null,
-    tags        text                            not null,
+    tags        text[]                          not null,
     image       varchar                         not null,
     cover       varchar                         not null,
     title       varchar                         not null,
@@ -31,7 +31,7 @@ create table public.schedules
     rows     integer                         not null,
     seats    integer                         not null,
     price    double precision                not null,
-    taken    text                            not null,
+    taken    text[]                          not null,
     "filmId" uuid
         constraint "FK_1c2f5e637713a429f4854024a76"
             references public.films
